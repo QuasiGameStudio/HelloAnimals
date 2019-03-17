04a8344ee19c22e1edf65b3e0dcc229cc4c5521e
januar elsan <januarelsan@gmail.com>
2019-03-17 14:09:01 +0700

    Init Commit

---
+++ Assets/Main/SceneController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class SceneController : Singleton<SceneController> {
+
+	[SerializeField] private string backToScene = "";
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			if(backToScene == "QuitGame"){
+				QuitGame();
+			}else if(backToScene == ""){
+								
+			}else{
+				GoToScene(backToScene);
+			}
+		} 
+	}
+
+
+	public void RestartScene (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	public void GoToScene(string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+
+	
+	
+	public void QuitGame(){
+		Application.Quit ();
+	}
+
+	
+}
